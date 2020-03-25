409d1d922e609d34c184fba652ac6ecc6098f486
zn41000 <nan4tian@gmail.com>
2020-03-25 16:47:40 +0800

    return disaster status

--- qiujidiwen.py
+++ qiujidiwen.py
@@ -3,11 +3,62 @@
 import netCDF4 as nc
 import numpy as np
 import datetime
-import time
 import cv2
-import pyexiv2
-from pyexiv2 import Image
-import PIL.Image as IImage
+
+
+def getTimeValue(self):
+
+    dateTimeNumList = []  # 准备存储time的值
+    dateTimeUnits = ''  # 准备记录初始时间
+    for key in self.variables.keys():  # 在变量中筛选出time变量的全部信息
+        if key.lower() in ['time', 't']:
+            dateTimeNumList = self.variables[key][:]  # 获取time字段全部值
+            dateTimeUnits = self.variables[key].units  # 获取初始时间信息
+            break
+
+    dateTimeStrList = []  # 该列表存储真实日期字符串
+    dayValueList = []  # 该列表存储日值数据对应的time字段值
+
+    # dateTimeUnits = 'hours since yyyymmddhhmmss'
+    startDateTimeStr = ' '.join(dateTimeUnits.split()[-1:])  # -1提取日期
+
+    stdt = datetime.datetime.strptime(
+        startDateTimeStr, '%Y%m%d%H%M%S')  # stdt=真实起始时间
+
+    if dateTimeUnits.split()[0] == 'hours':  # 0提取计时单位
+        for dtn in dateTimeNumList:  # 逐一提取time字段值
+            dt = stdt + datetime.timedelta(hours=dtn)  # 真实时间 = 起始时间 + 时间差
+            ymdH = dt.strftime('%Y-%m-%d')  # 生成真实日期字符串
+            H = dt.strftime('%H')  # 生成真实时间值
+            dateTimeStrList.append(ymdH)
+
+            # 取出每日20点的time字段值，并存入列表
+            if int(H) % 24 == 20:
+                dayValueList.append(dtn)
+
+    # 分别返回：真实日期列表，逐日time值列表，全部time值列表
+    return dateTimeStrList, dayValueList, dateTimeNumList
+
+
+def imgout(img_in, scatter, red, green, blue, img_out):  # 出图
+    img_gray = img_in * scatter  # 离散扩大到最大值为255
+    img_gray = img_gray.astype(np.uint8)
+    img_gray_rsz = cv2.resize(img_gray, (440, 340),
+                              interpolation=cv2.INTER_NEAREST)
+    img_rgba = cv2.cvtColor(img_gray_rsz, cv2.COLOR_GRAY2RGBA)
+    img_rgba[:, :, 3] = img_gray_rsz  # Alpha通道
+    img_rgba[:, :, 0] = blue  # B通道
+    img_rgba[:, :, 1] = green  # G通道
+    img_rgba[:, :, 2] = red  # R通道
+    cv2.imwrite(img_out, img_rgba)
+
+
+def disaster(img):  # 判断是否出现灾情
+    if np.max(img) > 0:
+        print(True)
+    else:
+        print(False)
+
 
 if __name__ == '__main__':
     # filename = sys.argv[1]
@@ -30,39 +81,6 @@ if __name__ == '__main__':
     var_min_data = f[var_min][:]  # 获取变量的数据
     var_min_data = np.array(var_min_data)  # 转化为np.array数组
 
-    def getTimeValue(self):
-
-        dateTimeNumList = []  # 准备存储time的值
-        dateTimeUnits = ''  # 准备记录初始时间
-        for key in self.variables.keys():  # 在变量中筛选出time变量的全部信息
-            if key.lower() in ['time', 't']:
-                dateTimeNumList = self.variables[key][:]  # 获取time字段全部值
-                dateTimeUnits = self.variables[key].units  # 获取初始时间信息
-                break
-
-        dateTimeStrList = []  # 该列表存储真实日期字符串
-        dayValueList = []  # 该列表存储日值数据对应的time字段值
-
-        # dateTimeUnits = 'hours since yyyymmddhhmmss'
-        startDateTimeStr = ' '.join(dateTimeUnits.split()[-1:])  # -1提取日期
-
-        stdt = datetime.datetime.strptime(
-            startDateTimeStr, '%Y%m%d%H%M%S')  # stdt=真实起始时间
-
-        if dateTimeUnits.split()[0] == 'hours':  # 0提取计时单位
-            for dtn in dateTimeNumList:  # 逐一提取time字段值
-                dt = stdt + datetime.timedelta(hours=dtn)  # 真实时间 = 起始时间 + 时间差
-                ymdH = dt.strftime('%Y-%m-%d')  # 生成真实日期字符串
-                H = dt.strftime('%H')  # 生成真实时间值
-                dateTimeStrList.append(ymdH)
-
-                # 取出每日20点的time字段值，并存入列表
-                if int(H) % 24 == 20:
-                    dayValueList.append(dtn)
-
-        # 分别返回：真实日期列表，逐日time值列表，全部time值列表
-        return dateTimeStrList, dayValueList, dateTimeNumList
-
     # 获取日值数据对应下标
     timelist = getTimeValue(f)[1]
     daylist = []
@@ -70,18 +88,18 @@ if __name__ == '__main__':
         day_in = np.argwhere(getTimeValue(f)[2] == day)  # week_in为np格式浮点数
         day_out = int(day_in[0])  # 转化为int整数
         daylist.append(day_out)
-    print(daylist)
+    # print(daylist)
 
     # 获取15天的日值数据
     TMax_daylist = []
     for d in daylist:
         TMax_daylist.append(var_max_data[d][:])
-    print(np.shape(TMax_daylist))
+    # print(np.shape(TMax_daylist))
 
     TMin_daylist = []
     for d in daylist:
         TMin_daylist.append(var_min_data[d][:])
-    print(np.shape(TMin_daylist))
+    # print(np.shape(TMin_daylist))
 
     # 获取日均值数据
     Tmean_list = (np.array(TMax_daylist) - np.array(TMin_daylist)) / 2
@@ -92,8 +110,8 @@ if __name__ == '__main__':
         diwen_ez.append(condition)  # 存入二值数据
     diwen_ez = np.array(diwen_ez)  # 便于后续计算
 
-    print(type(diwen_ez))
-    print(np.shape(diwen_ez))
+    # print(type(diwen_ez))
+    # print(np.shape(diwen_ez))
 
     diwen_list = []  # 用于将15天二值数据每三天求和，然后储存
     diwen_mark = []  # 记录每一次出现满足预警条件的日期下标
@@ -107,20 +125,20 @@ if __name__ == '__main__':
                 getTimeValue(f)[2] == getTimeValue(f)[1][x])
             diwen_mark.append(int(diwen_mark_in[0]))
 
-    print(diwen_mark)
+    # print(diwen_mark)
 
-    print(np.shape(diwen_list))
+    # print(np.shape(diwen_list))
 
     diwen_days = []  # 记录出现满足预警条件的日期
     for mark in diwen_mark:
         diwen_days.append(getTimeValue(f)[0][mark])
 
-    print(diwen_days)
+    # print(diwen_days)
 
     # 记录预警日期到文件中
     with open(out_txtpath, 'w+') as fp:
         thisday = getTimeValue(f)[0][0]
-        str_today = "今天的日期为：" + thisday + "\n"
+        str_today = "今天的日期为：\n" + thisday + "\n"
         str_alert = "出现低温预警的日期有：\n"
         fp.write(str_today)
         fp.write(str_alert)
@@ -133,14 +151,8 @@ if __name__ == '__main__':
         dw_alert = np.where(diwen > 2, 1, 0)  # 根据出现预警的次数二值化
         dw_alert_sum += dw_alert  # 叠加总次数为一张图（一个点最多出现13次）
 
+    # 判断灾情
+    disaster(dw_alert_sum)
+
     # 出图
-    dw_alert_sum = dw_alert_sum * 19  # 范围（0,1）
-    dw_alert_sum = dw_alert_sum.astype(np.uint8)
-    dw_alert_sum = cv2.resize(dw_alert_sum, (440, 340),
-                              interpolation=cv2.INTER_NEAREST)
-    img_rgba = cv2.cvtColor(dw_alert_sum, cv2.COLOR_GRAY2RGBA)
-    img_rgba[:, :, 3] = dw_alert_sum
-    img_rgba[:, :, 0] = 180
-    img_rgba[:, :, 1] = 0
-    img_rgba[:, :, 2] = 194
-    cv2.imwrite(out_imgpath, img_rgba)
+    imgout(dw_alert_sum, 19, 180, 0, 194, out_imgpath)
--- read_cell2.py
+++ read_cell2.py
@@ -3,28 +3,14 @@
 import netCDF4 as nc
 import numpy as np
 import datetime
-import time
-
-filename = 'C:/Users/Zn/Desktop/ncdata/2019110110.nc'   # .nc文件路径
-f = nc.Dataset(filename)  # 读取.nc文件，传入f中。此时f包含了该.nc文件的全部信息
-
-var_lat, var_lon, var_TMax24, var_TMin24, var_Ww12 = 'lat', 'lon', 'TMax24', 'TMin24', 'Ww12'
-var_lat_data, var_lon_data, var_TMax24_data, var_TMin24_data, var_Ww12_data = f[var_lat][
-    :], f[var_lon][:], f[var_TMax24][:], f[var_TMin24][:], f[var_Ww12][:]  # 获取变量的数据
-var_lat_data, var_lon_data, var_TMax24_data, var_TMin24_data, var_Ww12_data = np.array(var_lat_data), np.array(
-    var_lon_data), np.array(var_TMax24_data), np.array(var_TMin24_data), np.array(var_Ww12_data)  # 转化为np.array数组
-var_lon_data, var_lat_data = np.around(var_lon_data, decimals=2), np.around(
-    var_lat_data, decimals=2)  # 将经纬度数据统一保留两位小数
-print("经度可选项(保留两位小数):", var_lon_data)
-print("纬度可选项(保留两位小数):", var_lat_data)
 
 
 def getTimeValue(self):
 
     dateTimeNumList = []  # 准备存储time的值
-    dateTimeUnits = ''  # 准备记录初始时间
+    dateTimeUnits = ""  # 准备记录初始时间
     for key in self.variables.keys():  # 在变量中筛选出time变量的全部信息
-        if key.lower() in ['time', 't']:
+        if key.lower() in ["time", "t"]:
             dateTimeNumList = self.variables[key][:]  # 获取time字段全部值
             dateTimeUnits = self.variables[key].units  # 获取初始时间信息
             break
@@ -33,16 +19,15 @@ def getTimeValue(self):
     dayValueList = []  # 该列表存储日值数据对应的time字段值
 
     # dateTimeUnits = 'hours since yyyymmddhhmmss'
-    startDateTimeStr = ' '.join(dateTimeUnits.split()[-1:])  # -1提取日期
+    startDateTimeStr = " ".join(dateTimeUnits.split()[-1:])  # -1提取日期
 
-    stdt = datetime.datetime.strptime(
-        startDateTimeStr, '%Y%m%d%H%M%S')  # stdt=真实起始时间
+    stdt = datetime.datetime.strptime(startDateTimeStr, "%Y%m%d%H%M%S")  # stdt=真实起始时间
 
-    if dateTimeUnits.split()[0] == 'hours':  # 0提取计时单位
+    if dateTimeUnits.split()[0] == "hours":  # 0提取计时单位
         for dtn in dateTimeNumList:  # 逐一提取time字段值
             dt = stdt + datetime.timedelta(hours=dtn)  # 真实时间 = 起始时间 + 时间差
-            ymdH = dt.strftime('%Y-%m-%d')  # 生成真实日期字符串
-            H = dt.strftime('%H')  # 生成真实时间值
+            ymdH = dt.strftime("%Y-%m-%d")  # 生成真实日期字符串
+            H = dt.strftime("%H")  # 生成真实时间值
             dateTimeStrList.append(ymdH)
 
             # 取出每日20点的time字段值，并存入列表
@@ -53,46 +38,139 @@ def getTimeValue(self):
     return dateTimeStrList, dayValueList, dateTimeNumList
 
 
-# 获取一星期time数据对应下标
-daylist = getTimeValue(f)[1]
-week = daylist[:7]
-weeklist = []
-for day in week:
-    week_in = np.argwhere(getTimeValue(f)[2] == day)  # week_in为np格式浮点数
-    week_out = int(week_in[0])  # 转化为int整数
-    weeklist.append(week_out)
-print(weeklist)
+if __name__ == "__main__":
+    # filename = sys.argv[1]
+    # xlon = sys.argv[2]
+    # ylat = sys.argv[3]
 
-# 根据输入经纬度获取对应点坐标
-xlon = input("请输入经度:")
-ylat = input("请输入纬度:")
-lon_in = np.argwhere(var_lon_data == float(xlon))
-lat_in = np.argwhere(var_lat_data == float(ylat))
-#print("读取输入的经纬度对应下标：", lon_in, lat_in)
-lon_out, lat_out = int(lon_in[0]), int(lat_in[0])
-#print("输入的经纬度对应下标分别是：", lon_out, lat_out)
+    filename = "C:/Users/Zn/Desktop/WORK/2019120516.nc"  # .nc文件路径
 
+    # 读取.nc文件，传入f中。此时f包含了该.nc文件的全部信息
+    f = nc.Dataset(filename)
+    var_lat, var_lon, var_TMax24, var_TMin24, var_Ww12 = (
+        "lat",
+        "lon",
+        "TMax24",
+        "TMin24",
+        "Ww12",
+    )
 
-def findcell(t, x, y, attribute):  # 传入定位下标参数与目标属性，输出所需位置的属性值
-    attribute_week = []
-    for i in t:
-        attribute_day = attribute[i][y][x]
-        attribute_week.append(attribute_day)
+    var_lat_data, var_lon_data, var_TMax24_data, var_TMin24_data, var_Ww12_data = (
+        f[var_lat][:],
+        f[var_lon][:],
+        f[var_TMax24][:],
+        f[var_TMin24][:],
+        f[var_Ww12][:],
+    )  # 获取变量的数据
 
-    return attribute_week
+    var_lat_data, var_lon_data, var_TMax24_data, var_TMin24_data, var_Ww12_data = (
+        np.array(var_lat_data),
+        np.array(var_lon_data),
+        np.array(var_TMax24_data),
+        np.array(var_TMin24_data),
+        np.array(var_Ww12_data),
+    )  # 转化为np.array数组
 
+    var_lon_data, var_lat_data = (
+        np.around(var_lon_data, decimals=2),
+        np.around(var_lat_data, decimals=2),
+    )  # 将经纬度数据统一保留两位小数
 
-TMax_week = findcell(weeklist, lon_out, lat_out, var_TMax24_data)
-TMin_week = findcell(weeklist, lon_out, lat_out, var_TMin24_data)
-Ww_week = findcell(weeklist, lon_out, lat_out, var_Ww12_data)
+    print("经度可选项(保留两位小数):", var_lon_data)
+    print("纬度可选项(保留两位小数):", var_lat_data)
 
-Ww_week = np.array(Ww_week)
-Ww_week = Ww_week.astype(np.int32)
+    # 获取一星期time数据对应下标
+    daylist = getTimeValue(f)[1]
+    week = daylist[:7]
+    weeklist = []
+    for day in week:
+        week_in = np.argwhere(getTimeValue(f)[2] == day)  # week_in为np格式浮点数
+        week_out = int(week_in[0])  # 转化为int整数
+        weeklist.append(week_out)
+    print(weeklist)
 
-weather = {0: "晴", 1: "多云", 2: "阴", 3: "阵雨", 4: "雷阵雨", 5: "雷阵雨并伴有冰雹", 6: "雨夹雪", 7: "小雨", 8: "中雨", 9: "大雨", 10: "暴雨", 11: "大暴雨", 12: "特大暴雨", 13: "阵雪", 14: "小雪", 15: "中雪", 16: "大雪", 17: "暴雪", 18: "雾", 19: "冻雨", 20: "沙尘暴", 21: "小到中雨", 22: "中到大雨", 23: "大到暴雨", 24: "暴雨到大暴雨", 25: "大暴雨到特大暴雨", 26: "小到中雪", 27: "中到大雪",
-           28: "大到暴雪", 29: "浮尘", 30: "扬沙", 31: "强沙尘暴", 32: "尚未编码", 33: "尚未编码", 34: "尚未编码", 35: "尚未编码", 36: "尚未编码", 37: "尚未编码", 38: "尚未编码", 39: "尚未编码", 40: "尚未编码", 41: "尚未编码", 42: "尚未编码", 43: "尚未编码", 44: "尚未编码", 45: "尚未编码", 46: "尚未编码", 47: "尚未编码", 48: "尚未编码", 49: "尚未编码", 50: "尚未编码", 51: "尚未编码", 52: "尚未编码", 53: "霾"}
-Ww_week = [weather[x] if x in weather else x for x in Ww_week]
+    # 根据输入经纬度获取对应点坐标
+    xlon = input("请输入经度:")
+    ylat = input("请输入纬度:")
+    lon_in = np.argwhere(var_lon_data == float(xlon))
+    lat_in = np.argwhere(var_lat_data == float(ylat))
+    # print("读取输入的经纬度对应下标：", lon_in, lat_in)
+    lon_out, lat_out = int(lon_in[0]), int(lat_in[0])
+    # print("输入的经纬度对应下标分别是：", lon_out, lat_out)
 
-print("一周最高气温：", TMax_week)
-print("一周最低气温：", TMin_week)
-print("一周天气现象：", Ww_week)
+    def findcell(t, x, y, attribute):  # 传入定位下标参数与目标属性，输出所需位置的属性值
+        attribute_week = []
+        for i in t:
+            attribute_day = attribute[i][y][x]
+            attribute_week.append(attribute_day)
+
+        return attribute_week
+
+    TMax_week = findcell(weeklist, lon_out, lat_out, var_TMax24_data)
+    TMin_week = findcell(weeklist, lon_out, lat_out, var_TMin24_data)
+    Ww_week = findcell(weeklist, lon_out, lat_out, var_Ww12_data)
+
+    Ww_week = np.array(Ww_week)
+    Ww_week = Ww_week.astype(np.int32)
+
+    weather = {
+        0: "晴",
+        1: "多云",
+        2: "阴",
+        3: "阵雨",
+        4: "雷阵雨",
+        5: "雷阵雨并伴有冰雹",
+        6: "雨夹雪",
+        7: "小雨",
+        8: "中雨",
+        9: "大雨",
+        10: "暴雨",
+        11: "大暴雨",
+        12: "特大暴雨",
+        13: "阵雪",
+        14: "小雪",
+        15: "中雪",
+        16: "大雪",
+        17: "暴雪",
+        18: "雾",
+        19: "冻雨",
+        20: "沙尘暴",
+        21: "小到中雨",
+        22: "中到大雨",
+        23: "大到暴雨",
+        24: "暴雨到大暴雨",
+        25: "大暴雨到特大暴雨",
+        26: "小到中雪",
+        27: "中到大雪",
+        28: "大到暴雪",
+        29: "浮尘",
+        30: "扬沙",
+        31: "强沙尘暴",
+        32: "尚未编码",
+        33: "尚未编码",
+        34: "尚未编码",
+        35: "尚未编码",
+        36: "尚未编码",
+        37: "尚未编码",
+        38: "尚未编码",
+        39: "尚未编码",
+        40: "尚未编码",
+        41: "尚未编码",
+        42: "尚未编码",
+        43: "尚未编码",
+        44: "尚未编码",
+        45: "尚未编码",
+        46: "尚未编码",
+        47: "尚未编码",
+        48: "尚未编码",
+        49: "尚未编码",
+        50: "尚未编码",
+        51: "尚未编码",
+        52: "尚未编码",
+        53: "霾",
+    }
+    Ww_week = [weather[x] if x in weather else x for x in Ww_week]
+
+    print("一周最高气温：", TMax_week)
+    print("一周最低气温：", TMin_week)
+    print("一周天气现象：", Ww_week)
